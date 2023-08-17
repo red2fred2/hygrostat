1928cbd1cf539af0d0986c05a8df2d5c964a7323
red2fred2 <red2fred2base@live.com>
2023-08-16 19:01:37 -0500

    Move hardware init out to hardware class

---
+++ src/hardware.rs
@@ -0,0 +1,63 @@
+//! Handles most low level hardware abstraction
+
+use cortex_m::delay::Delay;
+use rp2040_hal::{clocks::init_clocks_and_plls, pac, usb::UsbBus, Clock, Sio, Watchdog};
+use usb_device::class_prelude::UsbBusAllocator;
+
+use crate::{usb_manager::UsbManager, PIN1, USB_BUS, USB_MANAGER};
+
+pub struct Hardware {
+    pub delay: Delay,
+}
+
+impl Hardware {
+    pub fn init() -> Hardware {
+        let mut pac = pac::Peripherals::take().unwrap();
+        let core = pac::CorePeripherals::take().unwrap();
+        let mut watchdog = Watchdog::new(pac.WATCHDOG);
+        let sio = Sio::new(pac.SIO);
+
+        let external_xtal_freq_hz = 12_000_000u32;
+        let clocks = init_clocks_and_plls(
+            external_xtal_freq_hz,
+            pac.XOSC,
+            pac.CLOCKS,
+            pac.PLL_SYS,
+            pac.PLL_USB,
+            &mut pac.RESETS,
+            &mut watchdog,
+        )
+        .ok()
+        .unwrap();
+
+        let delay;
+
+        unsafe {
+            delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
+
+            USB_BUS = Some(UsbBusAllocator::new(UsbBus::new(
+                pac.USBCTRL_REGS,
+                pac.USBCTRL_DPRAM,
+                clocks.usb_clock,
+                true,
+                &mut pac.RESETS,
+            )));
+
+            USB_MANAGER = Some(UsbManager::new(USB_BUS.as_ref().unwrap()));
+            // Enable the USB interrupt
+            pac::NVIC::unmask(rp2040_hal::pac::Interrupt::USBCTRL_IRQ);
+        };
+
+        let pins = rp2040_hal::gpio::Pins::new(
+            pac.IO_BANK0,
+            pac.PADS_BANK0,
+            sio.gpio_bank0,
+            &mut pac.RESETS,
+        );
+
+        // Pin setup
+        unsafe { PIN1 = Some(pins.gpio0.into_push_pull_output()) };
+
+        Hardware { delay }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -9,22 +9,19 @@ static BOOT_LOADER: [u8; 256] = rp2040_boot2::BOOT_LOADER_GENERIC_03H;
 #[cfg(feature = "rt")]
 extern crate cortex_m_rt;
 
+mod hardware;
 mod serial_logger;
 mod usb_manager;
 
-use cortex_m::delay::Delay;
 use embedded_hal::digital::v2::OutputPin;
+use hardware::Hardware;
 use log::{debug, error, info, trace, warn};
 use panic_reset as _;
 use rp2040_hal::{
-    clocks::{init_clocks_and_plls, Clock},
     entry,
     gpio::{bank0::Gpio0, Output, Pin, PushPull},
-    pac,
     pac::interrupt,
-    sio::Sio,
     usb::UsbBus,
-    watchdog::Watchdog,
 };
 use serial_logger::SerialLogger;
 use usb_device;
@@ -32,7 +29,6 @@ use usb_device::bus::UsbBusAllocator;
 
 use crate::usb_manager::UsbManager;
 
-static mut DELAY: Option<Delay> = None;
 static mut LOGGER: Option<SerialLogger> = None;
 static LOG_LEVEL: log::LevelFilter = log::LevelFilter::Trace;
 static mut PIN1: Option<Pin<Gpio0, Output<PushPull>>> = None;
@@ -50,53 +46,7 @@ unsafe fn USBCTRL_IRQ() {
 
 #[entry]
 fn main() -> ! {
-    // Set up hardware
-    let mut pac = pac::Peripherals::take().unwrap();
-    let core = pac::CorePeripherals::take().unwrap();
-    let mut watchdog = Watchdog::new(pac.WATCHDOG);
-    let sio = Sio::new(pac.SIO);
-
-    let external_xtal_freq_hz = 12_000_000u32;
-    let clocks = init_clocks_and_plls(
-        external_xtal_freq_hz,
-        pac.XOSC,
-        pac.CLOCKS,
-        pac.PLL_SYS,
-        pac.PLL_USB,
-        &mut pac.RESETS,
-        &mut watchdog,
-    )
-    .ok()
-    .unwrap();
-
-    unsafe {
-        DELAY = Some(cortex_m::delay::Delay::new(
-            core.SYST,
-            clocks.system_clock.freq().to_Hz(),
-        ));
-
-        USB_BUS = Some(UsbBusAllocator::new(UsbBus::new(
-            pac.USBCTRL_REGS,
-            pac.USBCTRL_DPRAM,
-            clocks.usb_clock,
-            true,
-            &mut pac.RESETS,
-        )));
-
-        USB_MANAGER = Some(UsbManager::new(USB_BUS.as_ref().unwrap()));
-        // Enable the USB interrupt
-        pac::NVIC::unmask(rp2040_hal::pac::Interrupt::USBCTRL_IRQ);
-    };
-
-    let pins = rp2040_hal::gpio::Pins::new(
-        pac.IO_BANK0,
-        pac.PADS_BANK0,
-        sio.gpio_bank0,
-        &mut pac.RESETS,
-    );
-
-    // Pin setup
-    unsafe { PIN1 = Some(pins.gpio0.into_push_pull_output()) };
+    let mut hardware = Hardware::init();
 
     // Set up logging
     unsafe {
@@ -109,12 +59,12 @@ fn main() -> ! {
     let mut number = 0;
 
     loop {
-        info!("Number: {number}");
+        info!("Hewwo: {number}");
 
         unsafe {
             PIN1.as_mut().unwrap().set_high().unwrap();
-            DELAY.as_mut().unwrap().delay_ms(1000)
         };
+        hardware.delay.delay_ms(1000);
 
         error!("Error");
         warn!("Warning");
@@ -125,7 +75,7 @@ fn main() -> ! {
 
         unsafe {
             PIN1.as_mut().unwrap().set_low().unwrap();
-            DELAY.as_mut().unwrap().delay_ms(1000)
         };
+        hardware.delay.delay_ms(1000);
     }
 }
